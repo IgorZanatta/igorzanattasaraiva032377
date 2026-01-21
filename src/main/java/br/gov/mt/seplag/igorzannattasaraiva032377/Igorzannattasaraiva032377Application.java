471ba9964a434436e32260d37c28ba652331d74d
zanat <zanatta2014@outlook.com>
2026-01-20 23:32:13 -0300

    chore: bootstrap Spring Boot project

---
+++ src/main/java/br/gov/mt/seplag/igorzannattasaraiva032377/Igorzannattasaraiva032377Application.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.igorzannattasaraiva032377;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Igorzannattasaraiva032377Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Igorzannattasaraiva032377Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/mt/seplag/igorzannattasaraiva032377/Igorzannattasaraiva032377ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.igorzannattasaraiva032377;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Igorzannattasaraiva032377ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
