eed961815d6dbeab8d14c802b548b79edc2c1673
zanat <zanatta2014@outlook.com>
2026-01-27 20:20:46 -0300

    fix(security): update WebSecurityConfig to match versioned API endpoints

--- src/main/java/br/gov/mt/seplag/igorzannattasaraiva032377/security/WebSecurityConfig.java
+++ src/main/java/br/gov/mt/seplag/igorzannattasaraiva032377/security/WebSecurityConfig.java
@@ -61,8 +61,8 @@ public class WebSecurityConfig {
             .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                .requestMatchers("api/v1/auth/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                 .anyRequest().authenticated());
 
         http.authenticationProvider(authenticationProvider);
