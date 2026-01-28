ae252dc1e124901ee851ceaa69109303dd7cc0a8
zanat <zanatta2014@outlook.com>
2026-01-28 00:03:35 -0300

    refactor(genreRepository): reorganize import statements for clarity

--- src/main/java/br/gov/mt/seplag/igorzannattasaraiva032377/repository/genre/GenreRepository.java
+++ src/main/java/br/gov/mt/seplag/igorzannattasaraiva032377/repository/genre/GenreRepository.java
@@ -1,12 +1,18 @@
 package br.gov.mt.seplag.igorzannattasaraiva032377.repository.genre;
 
-import br.gov.mt.seplag.igorzannattasaraiva032377.entity.genre.GenreEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.gov.mt.seplag.igorzannattasaraiva032377.entity.genre.GenreEntity;
+
 public interface GenreRepository extends JpaRepository<GenreEntity, UUID> {
+
     Optional<GenreEntity> findByName(String name);
+
     boolean existsByName(String name);
+
+    List<GenreEntity> findByNameContainingIgnoreCase(String name);
 }
