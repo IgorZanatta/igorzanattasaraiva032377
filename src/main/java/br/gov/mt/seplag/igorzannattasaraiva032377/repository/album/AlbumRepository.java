572d3f7f7f8bc879a706e76aa4f6f41e13885fd3
zanat <zanatta2014@outlook.com>
2026-01-31 22:06:50 -0300

    feat(album): add findByArtistType method to retrieve albums by artist type

--- src/main/java/br/gov/mt/seplag/igorzannattasaraiva032377/repository/album/AlbumRepository.java
+++ src/main/java/br/gov/mt/seplag/igorzannattasaraiva032377/repository/album/AlbumRepository.java
@@ -6,8 +6,11 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.gov.mt.seplag.igorzannattasaraiva032377.entity.album.AlbumEntity;
+import br.gov.mt.seplag.igorzannattasaraiva032377.entity.artist.ArtistType;
 
 public interface AlbumRepository extends JpaRepository<AlbumEntity, UUID> {
 
@@ -18,4 +21,8 @@ public interface AlbumRepository extends JpaRepository<AlbumEntity, UUID> {
     Page<AlbumEntity> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
     Page<AlbumEntity> findByReleaseYear(Integer releaseYear, Pageable pageable);
+
+    @Query(value = "select distinct aa.album from ArtistAlbumEntity aa where aa.artist.type = :type",
+           countQuery = "select count(distinct aa.album) from ArtistAlbumEntity aa where aa.artist.type = :type")
+    Page<AlbumEntity> findByArtistType(@Param("type") ArtistType type, Pageable pageable);
 }
